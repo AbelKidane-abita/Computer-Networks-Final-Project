abd12cfe6088c3522b1d3300022b13c3968ff103
Abel Kidane Haile <65540326+AbelKidane-abita@users.noreply.github.com>
2023-04-27 16:13:42 +0400

    gh

--- src/Server/Server_Main.java
+++ src/Server/Server_Main.java
@@ -1,16 +1,34 @@
 package Server;
 
+import java.net.DatagramPacket;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Server_Main {
-	
+
 	private static Server ServerName;
 	
+	// linked lists to store
+	LinkedList<Server> ClientsList = new LinkedList<Server>();
+	Queue<DatagramPacket> myQueue = new LinkedList<DatagramPacket>();
+
+	private static void Organizer() {
+		//use setReceiveddata to update the value of receiveData
+		
+		
+	}
 	
+	//
+	private static void ReceivePacketsUsingThread() {
+				
+		
+	}
 	
-	
+
 	public static void main(String[] args) {
-		
-		
-		
+
+
+
 	}
 
 }
